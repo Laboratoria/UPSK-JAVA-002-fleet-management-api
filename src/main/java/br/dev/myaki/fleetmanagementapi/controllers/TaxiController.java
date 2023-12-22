e99304cabf93a307d3bb99ed0ba1fd3c216bb9db
Juliana Myaki <j.myaki@gmail.com>
2023-12-22 15:49:25 -0300

    add Pageable interface

--- src/main/java/br/dev/myaki/fleetmanagementapi/controllers/TaxiController.java
+++ src/main/java/br/dev/myaki/fleetmanagementapi/controllers/TaxiController.java
@@ -3,10 +3,10 @@ package br.dev.myaki.fleetmanagementapi.controllers;
 import br.dev.myaki.fleetmanagementapi.models.Taxi;
 import br.dev.myaki.fleetmanagementapi.repositories.TaxiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class TaxiController {
@@ -14,8 +14,7 @@ public class TaxiController {
     @Autowired
     private TaxiRepository taxiRepository;
     @GetMapping("/taxis")
-    public List<Taxi> get(){
-
-        return taxiRepository.findAll();
+    public Page<Taxi> findAllTaxis(Pageable pageable){
+        return taxiRepository.findAll(pageable);
     }
 }
