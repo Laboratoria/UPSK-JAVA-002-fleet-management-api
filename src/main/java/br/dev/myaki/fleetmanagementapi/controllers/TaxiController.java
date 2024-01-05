1a449c3627c76992f3fd08f1a626219754d738e8
Juliana Myaki <j.myaki@gmail.com>
2024-01-05 12:41:17 -0300

    Implement @ResponseBody

--- src/main/java/br/dev/myaki/fleetmanagementapi/controllers/TaxiController.java
+++ src/main/java/br/dev/myaki/fleetmanagementapi/controllers/TaxiController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,8 +18,7 @@ public class TaxiController {
     private TaxiRepository taxiRepository;
     @GetMapping("/taxis")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<Page<Taxi>> findAllTaxis(Pageable pageable){
-
-        return ResponseEntity.status(HttpStatus.OK).body(taxiRepository.findAll(pageable));
+    public @ResponseBody Page<Taxi> findAllTaxis(Pageable pageable){
+        return taxiRepository.findAll(pageable);
     }
 }
