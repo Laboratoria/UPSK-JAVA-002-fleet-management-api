2783d88db338b14f45f5dabac535a014463cf086
Juliana Myaki <j.myaki@gmail.com>
2024-01-05 10:01:57 -0300

    Fix imports

--- src/main/java/br/dev/myaki/fleetmanagementapi/controllers/TaxiController.java
+++ src/main/java/br/dev/myaki/fleetmanagementapi/controllers/TaxiController.java
@@ -5,7 +5,11 @@ import br.dev.myaki.fleetmanagementapi.repositories.TaxiRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +18,9 @@ public class TaxiController {
     @Autowired
     private TaxiRepository taxiRepository;
     @GetMapping("/taxis")
-    public Page<Taxi> findAllTaxis(Pageable pageable){
-        return taxiRepository.findAll(pageable);
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Page<Taxi>> findAllTaxis(Pageable pageable){
+
+        return ResponseEntity.status(HttpStatus.OK).body(taxiRepository.findAll(pageable));
     }
 }
